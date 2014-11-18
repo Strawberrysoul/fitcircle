4401335d8fdfaf2984218052a9f6a1c467a1533d
Jan Sauerwein <hska@jsauerwein.de>
2014-11-18 06:04:19 +0100

    Code for exercise 2

---
+++ app/src/main/java/de/jsauerwein/fitcircle/TrainingScheduleOverview.java
@@ -0,0 +1,46 @@
+package de.jsauerwein.fitcircle;
+
+
+import android.os.Bundle;
+import android.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+/**
+ *
+ */
+public class TrainingScheduleOverview extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.main_trainingschedule_overview_fragment, container, false);
+
+        return rootView;
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        final ListView exerciseList = (ListView) this.getActivity().findViewById(R.id.main_trainingschedule_exercises);
+
+        final List<Exercise> exercises = new ArrayList<Exercise>();
+        exercises.add(new Exercise.Builder().type(3).name("Exercise 3").addTool(1).build());
+        exercises.add(new Exercise.Builder().type(14).name("Exercise 14").addTool(1).build());
+        exercises.add(new Exercise.Builder().type(21).name("Exercise 21").addTool(1).build());
+        exercises.add(new Exercise.Builder().type(28).name("Exercise 28").addTool(1).build());
+        exercises.add(new Exercise.Builder().type(39).name("Exercise 39").addTool(4).build());
+        exercises.add(new Exercise.Builder().type(41).name("Exercise 41").addTool(3).build());
+        exercises.add(new Exercise.Builder().type(44).name("Exercise 44").addTool(2).addTool(4).build());
+        exercises.add(new Exercise.Builder().type(45).name("Exercise 45").addTool(2).addTool(3).build());
+
+        exerciseList.setAdapter(new ExerciseAdapter(this.getActivity(), exercises));
+    }
+}
